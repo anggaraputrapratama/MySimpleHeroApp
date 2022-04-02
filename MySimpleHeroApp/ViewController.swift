655e5c5167a07ee35c51331131b80a477a0e4640
Anggara Putra Pratama <anggaraputraa.pratama@gmail.com>
2022-04-02 21:26:51 +0700

    Initial Commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Anggara Putra Pratama on 02/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
